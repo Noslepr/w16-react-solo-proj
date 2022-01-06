15a45ac1bdf9f98a78e4ed9547ddcf002bd84c2a
Chris Young <ChrisC.Young1@gmail.com>
2022-01-06 12:48:09 -0800

    created login POST api/session route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,36 +1,21 @@
 const asyncHandler = require('express-async-handler')
 const router = require('express').Router();
 
-const { setTokenCookie } = require('../../utils/auth.js')
-const { User } = require('../../db/models')
-const { restoreUser, requireAuth } = require('../../utils/auth.js')
+const sessionRouter = require('./session')
+const usersRouter = require('./users')
 
+// const { setTokenCookie } = require('../../utils/auth.js')
+// const { User } = require('../../db/models')
+// const { restoreUser, requireAuth } = require('../../utils/auth.js')
 
-// //test api routes
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-// });
+router.use('/session', sessionRouter)
+router.use('/uses', usersRouter)
 
-// // test route for setting token cookie
-// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     })
-//     setTokenCookie(res, user);
-//     return res.json({ user })
-// }))
+//test api routes
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
 
-// // test route for restoreUser middleware
-// router.get('/restore-user', restoreUser, (req, res) => {
-//     return res.json(req.user)
-// })
-
-// // test route for requireAuth
-// router.get('/require-auth', requireAuth, (req, res) => {
-//     return res.json(req.user)
-// })
 
 
 module.exports = router;
