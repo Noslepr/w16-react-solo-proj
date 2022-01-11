9f8016b31f83d486e8f7b68033ce05a0eb750e29
Chris Young <ChrisC.Young1@gmail.com>
2022-01-11 09:34:22 -0800

    backend route working without errorhandling

--- frontend/src/store/photos.js
+++ frontend/src/store/photos.js
@@ -1,11 +1,19 @@
 import { csrfFetch } from './csrf'
 
 const GET_PHOTOS = 'photos/GET_PHOTOS'
+const POST_PHOTO = 'photos/POST_PHOTO'
 
 const get = (photos) => {
     return {
         type: GET_PHOTOS,
         photos
+    }
+}
+
+const post = (photo) => {
+    return {
+        type: POST_PHOTO,
+        photo
     }
 }
 
@@ -18,6 +26,12 @@ export const getPhotos = () => async dispatch => {
     dispatch(get(data.photos))
 }
 
+export const postPhoto = (photo) => async dispatch => {
+    const res = await csrfFetch('/api/photos', {
+        method: 'POST',
+        body: JSON.stringify({})
+    })
+}
 
 const initialState = { photos: null }
 const photoReducer = (state = initialState, action) => {
