5cb57f670a89f7326c5752e94676bb8371c99840
Chris Young <ChrisC.Young1@gmail.com>
2022-01-13 17:31:22 -0800

    climb text

--- frontend/src/conponents/Splash/index.js
+++ frontend/src/conponents/Splash/index.js
@@ -2,21 +2,34 @@ import React, { useState } from 'react';
 
 import { Modal } from '../../context/Modal';
 import SignupForm from '../SignupFormPage/SignupForm';
+import LoginForm from '../LoginFormModal/LoginForm';
 import './Splash.css'
 
 
 const Splash = () => {
-    const [showModal, setShowModal] = useState(false);
+    const [signupModal, setSignupModal] = useState(false);
+    const [loginModal, setLoginModal] = useState(false);
 
     return (
         <div id='splash-background'>
-            <h1 className='splash-text'>Find your inspiration.</h1>
+            <h1 className='splash-text'>Climb your inspiration.</h1>
             <h2 className='splash-text'>Join the Rock Flickr community and explore <br>
             </br>...tens of Rock Climbing photos</h2>
-            <button id='splash-signup' onClick={() => setShowModal(true)}>Start for free</button>
-                {showModal && (
-                    <Modal onClose={() => setShowModal(false)}>
-                        <SignupForm />
+            <button id='splash-signup' onClick={() => setSignupModal(true)}>Start for free</button>
+                {signupModal && (
+                    <Modal onClose={() => setSignupModal(false)}>
+                        <SignupForm
+                            setLoginModal={setLoginModal}
+                            setSignupModal={setSignupModal}
+                        />
+                    </Modal>
+                )}
+                {loginModal && (
+                    <Modal onClose={() => setLoginModal(false)}>
+                        <LoginForm
+                            setLoginModal={setLoginModal}
+                            setSignupModal={setSignupModal}
+                        />
                     </Modal>
                 )}
         </div>
